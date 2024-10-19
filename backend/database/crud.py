c28739d5c072eb70738781b6658abc07269cbb33
Rodaldrey <rodaldrey@icloud.com>
2024-10-18 20:27:14 -0500

    admin and chat crud add

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -28,3 +28,58 @@ def mentor_delete(db: Session, mentor_id: int):
     db.delete(mentor)
     db.commit()
     return mentor
+
+
+def admin_create(db: Session, admin: tables.Admin):
+    db_admin = tables.Admin(name=admin.name, phone_num=admin.phone_num, email_id=admin.email_id, username=admin.username, password=admin.password)
+    db.add(db_admin)
+    db.commit()
+    db.refresh(db_admin)
+    return admin
+
+def admin_get_all(db: Session):
+    return db.query(tables.Admin).all()
+
+def admin_get_one(db: Session, admin_id: int):
+    return db.query(tables.Admin).filter(tables.Admin.id == admin_id).one()
+
+def admin_update(db: Session, admin_id: int, admin: tables.Admin):
+    update_query = {tables.Admin.name: admin.name, tables.Admin.phone_num: admin.phone_num, tables.Admin.email_id: admin.email_id, tables.Admin.username: admin.username, tables.Admin.password: admin.password}
+    db.query(tables.Admin).filter(tables.Admin.id == admin.id).update(update_query)
+    db.commit()
+    return db.query(tables.Admin).filter(tables.Admin.id == admin.id).one()
+
+def admin_delete(db: Session, admin_id: int):
+    admin = db.query(tables.Admin).filter(tables.Admin.id == admin_id).first()
+    if not admin:
+        return None
+    db.delete(admin)
+    db.commit()
+    return admin
+
+def chat_create(db: Session, chat: tables.Chat):
+    db_chat = tables.Chat(chat_id=chat.chat_id, mentor_id=chat.mentor_id, mentee_id=chat.mentee_id)
+    db.add(db_chat)
+    db.commit()
+    db.refresh(db_chat)
+    return chat
+
+def chat_get_all(db: Session):
+    return db.query(tables.Chat).all()
+
+def chat_get_one(db: Session, chat_id: int):
+    return db.query(tables.Chat).filter(tables.Chat.chat_id == chat_id).one()
+
+def chat_update(db: Session, chat_id: int, chat: tables.Chat):
+    update_query = {tables.Chat.chat_id: chat.chat_id, tables.Chat.mentor_id: chat.mentor_id, tables.Chat.mentee_id: chat.mentee_id}
+    db.query(tables.Chat).filter(tables.Chat.chat_id == chat.chat_id).update(update_query)
+    db.commit()
+    return db.query(tables.Chat).filter(tables.Chat.chat_id == chat.chat_id).one()
+
+def chat_delete(db: Session, chat_id: int):
+    chat = db.query(tables.Chat).filter(tables.Chat.chat_id == chat_id).first()
+    if not chat:
+        return None
+    db.delete(chat)
+    db.commit()
+    return chat

761b2cc13e21cd213599984fee47788d8da1deea
Kallen525 <kallenwork1@gmail.com>
2024-10-18 20:52:37 -0500

    Created the login/SignUp. First time really going in with SVG and it is very interesting.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
-import LandingPage from './pages/index';
+import LandingPage from "./pages/index";
 import ChatPage from "./pages/chat";
-
+import LoginSignIn from "./pages/LoginSignUp";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/LoginSignup" element={<LoginSignIn />} />
       </Routes>
     </Router>
   );

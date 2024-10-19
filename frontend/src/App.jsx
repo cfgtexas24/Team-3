e9c1818dbe3a995b612696ee8daaaa07d1c4cd20
Affan1khan3 <126931850+Affan1khan3@users.noreply.github.com>
2024-10-18 22:21:38 -0500

    Added mock testimonial feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,6 +60,7 @@ const App = () => {
 };
 
 export default App;*/
+/*This one works UNDER
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./index.css";
 
@@ -71,7 +72,30 @@ const App = () => {
   return (
     <Router>
       <FlipNavWrapper />
-      <SwipeCarousel /> {/* Include the SwipeCarousel component here */}
+      <SwipeCarousel /> {/* Include the SwipeCarousel component here }
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add other routes here if needed }
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;*/
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./index.css";
+
+import LandingPage from './pages/index';
+import FlipNavWrapper from './components/navbar'; // Adjust the path if necessary
+import { SwipeCarousel } from './components/swipe'; // Adjust the path if necessary
+import ScrollingTestimonials from './components/ScrollingTestimonials'; // Adjust the path if necessary
+
+const App = () => {
+  return (
+    <Router>
+      <FlipNavWrapper />
+      <SwipeCarousel />
+      <ScrollingTestimonials /> {/* Include the ScrollingTestimonials component */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         {/* Add other routes here if needed */}
@@ -81,4 +105,5 @@ const App = () => {
 };
 
 export default App;
+
 

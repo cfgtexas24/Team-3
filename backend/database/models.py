3f72100bb59f310fbc7123e1fec7c52a2d6f010e
Rodaldrey <rodaldrey@icloud.com>
2024-10-18 21:51:28 -0500

    models for admin, emergency and mentee

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+#model for mentor
 class Mentor(BaseModel):
     id: int
     name: str
@@ -14,4 +15,43 @@ class Mentor(BaseModel):
     class Config:
         orm_mode = True
 
+#model for admin
+class Admin(BaseModel):
+    id: int
+    name: str
+    phone_num: int
+    email_id: str
+    username: str
+    password: str
+    email: str
+    notifications_id: int
 
+    class Config:
+        orm_mode = True
+
+#model for chat
+class Emergency(BaseModel):
+    id: int
+    name: str
+    caller_phone_num: int
+    staff_phone_num: int
+    message: str
+
+    class Config:
+        orm_mode = True
+
+#model for chat
+class Mentee(BaseModel):
+    id: int
+    name: str
+    mentored: bool
+    sheltered: bool
+    email: str
+    age: int
+    username: str
+    password: str
+    screened: bool
+    chats: str
+
+    class Config:
+        orm_mode = True

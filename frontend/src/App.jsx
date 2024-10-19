463bf563fa68bcd6221d495a5f92178a905e5ac4
Kallen525 <kallenwork1@gmail.com>
2024-10-19 06:51:42 -0500

    added updated look to Emergency page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
-import LandingPage from './pages/index';
+import LandingPage from "./pages/index";
 import ChatPage from "./pages/chat";
 import AuthPage from "./pages/auth";
 import MentorPage from "./pages/mentor";
+import EmergencyPage from "./pages/Emergency";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/mentor" element={<MentorPage />} />
+        <Route path="/emergency" element={<EmergencyPage />} />
       </Routes>
     </Router>
   );
